2ce83a6f4f5ff29b4d8f5fb3e53f29097e9e2032
l2xu <weihrauch.business@gmail.com>
2023-01-24 14:39:37 +0100

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import '@/styles/globals.css'
+import Sidebar from '@/components/Sidebar'
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <>
+    <Sidebar />
+      <main>
+        <div className="mt-6  ml-24 mr-4 lg:mr-16 lg:ml-32">
+          <Component {...pageProps} />
+        </div>
+      </main></>
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,10 @@ import React from "react";
 
 export default function Home() {
   return (
-    <>
-      <h1 className="text-2xl font-bold">Hello NextJS & Tailwind</h1>
-      Sidebar
+    <><h1 className="text-3xl font-bold mb-16"> Sidebar with TailwindCSS</h1>
+
+    <h2 className="text-2xl font-bold mb-6">Hello Tailwind</h2>
+      
     </>
   );
 }
